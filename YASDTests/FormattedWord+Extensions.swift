08c749c075036d8579abbf9b99eb49916a176c0a
Vyacheslav Konopkin <robotal76@gmail.com>
2021-05-07 17:58:55 +0300

    Add BookmarksViewModel tests
    testSearch, testRestore

--- YASDTests/FormattedWord+Extensions.swift
+++ YASDTests/FormattedWord+Extensions.swift
@@ -1,15 +1,16 @@
-////
-////  FormattedWord+Extensions.swift
-////  YASDTests
-////
-////  Created by Vyacheslav Konopkin on 14.05.2020.
-////  Copyright © 2020 yac. All rights reserved.
-////
 //
-//import Foundation
+//  FormattedWord+Extensions.swift
+//  YASDTests
 //
-//extension FormattedWord {
-//    public init(_ header: String) {
-//        self.init(header: header, formatted: NSAttributedString(), soundUrl: nil, definition: "")
-//    }
-//}
+//  Created by Vyacheslav Konopkin on 14.05.2020.
+//  Copyright © 2020 yac. All rights reserved.
+//
+
+@testable import YASD
+import Foundation
+
+extension FormattedWord {
+    public init(_ header: String) {
+        self.init(header: header, formatted: NSAttributedString(), soundUrl: nil, definition: "")
+    }
+}
--- YASDTests/ViewModel/BookmarksViewModelTests.swift
+++ YASDTests/ViewModel/BookmarksViewModelTests.swift
@@ -1,200 +1,130 @@
-////
-////  BookmarksViewModelTests.swift
-////  YASDTests
-////
-////  Created by Vyacheslav Konopkin on 14.05.2020.
-////  Copyright © 2020 yac. All rights reserved.
-////
 //
-//@testable import YASD
+//  BookmarksViewModelTests.swift
+//  YASDTests
 //
-//import XCTest
-//import RxSwift
-//import RxCocoa
-//import RxTest
-//import Cuckoo
+//  Created by Vyacheslav Konopkin on 14.05.2020.
+//  Copyright © 2020 yac. All rights reserved.
 //
-//class BookmarksViewModelTests: XCTestCase {
-//    enum TestError: Error {
-//        case someError
-//    }
-//
-//    let disposeBag = DisposeBag()
-//    let scheduler = TestScheduler(initialClock: 0)
-//
-//    // Inputs
-//    let errorUrl = "error"
-//    lazy var inputWords = scheduler.createHotObservable([
-//        .next(200, "test2"),
-//        .next(250, "test3"),
-//        .next(300, "test"),
-//        .completed(400)
-//    ]).asDriver(onErrorJustReturn: "")
-//    lazy var inputRestore = scheduler.createHotObservable([
-//        .next(200, "test2"),
-//        .next(250, "test3"),
-//        .next(300, "test"),
-//        .completed(400)
-//    ]).asDriver(onErrorJustReturn: "")
-//    lazy var inputUrls = scheduler.createHotObservable([
-//        .next(150, "some_url"),
-//        .next(200, "some_url_2"),
-//        .next(300, errorUrl),
-//        .completed(400)
-//    ]).asDriver(onErrorJustReturn: "")
-//    lazy var inputBookmarks = scheduler.createHotObservable([
-//        .next(150, 0),
-//        .next(200, 0),
-//        .completed(400)
-//    ]).asDriver(onErrorJustReturn: -1)
-//
-//    // Outputs
-//    lazy var bookmarks = scheduler.createObserver(FormattedWordResult.self)
-//    lazy var played = scheduler.createObserver(PlayerManagerResult.self)
-//
-//    func testSearch() {
-//        // Arrange
-//        let data = createBookmarksMock([FormattedWord("test2"), FormattedWord("test3")])
-//        let viewModel = BookmarksViewModel(bookmarks: data,
-//                                           player: PlayerManagerStub(),
-//                                           spotlight: ExternalCacheServiceStub())
-//        let output = viewModel.transform(from: BookmarksViewModel.Input(search: inputWords,
-//                                                                        playUrl: Driver.never(),
-//                                                                        removeBookmark: Driver.never(),
-//                                                                        restore: Driver.never()))
-//        output.bookmarks.drive(bookmarks)
-//            .disposed(by: disposeBag)
-//
-//        // Act
-//        scheduler.start()
-//
-//        // Assert
-//        XCTAssertEqual(bookmarks.events, [
-//            .next(200, .success([FormattedWord("test2")])),
-//            .next(250, .success([FormattedWord("test3")])),
-//            .next(300, .success([FormattedWord("test2"), FormattedWord("test3")]))
-//        ])
-//    }
-//
-//    func testRestore() {
-//        // Arrange
-//        let data = createBookmarksMock([FormattedWord("test2"), FormattedWord("test3")])
-//        let viewModel = BookmarksViewModel(bookmarks: data,
-//                                           player: PlayerManagerStub(),
-//                                           spotlight: createSpotlightMock())
-//        let output = viewModel.transform(from: BookmarksViewModel.Input(search: Driver.never(),
-//                                                                        playUrl: Driver.never(),
-//                                                                        removeBookmark: Driver.never(),
-//                                                                        restore: inputRestore))
-//        output.bookmarks.drive(bookmarks)
-//            .disposed(by: disposeBag)
-//
-//        // Act
-//        scheduler.start()
-//
-//        // Assert
-//        XCTAssertEqual(bookmarks.events, [
-//            .next(200, .success([FormattedWord("test2")])),
-//            .next(250, .success([FormattedWord("test3")])),
-//            .next(300, .success([FormattedWord("test2"), FormattedWord("test3")]))
-//        ])
-//    }
-//
-//    func testPlay() {
-//        // Arrange
-//        let error = TestError.someError
-//        let viewModel = BookmarksViewModel(bookmarks: createBookmarksMock([]),
-//                                           player: MockFactory.createMockPlayerManager(errorUrl: errorUrl, error: error),
-//                                           spotlight: ExternalCacheServiceStub())
-//        let output = viewModel.transform(from: BookmarksViewModel.Input(search: Driver.never(),
-//                                                                        playUrl: inputUrls,
-//                                                                        removeBookmark: Driver.never(),
-//                                                                        restore: Driver.never()))
-//        output.played.drive(played)
-//            .disposed(by: disposeBag)
-//
-//        // Act
-//        scheduler.start()
-//
-//        // Assert
-//        XCTAssertEqual(played.events, [
-//            .next(150, .success(true)),
-//            .next(200, .success(true)),
-//            .next(300, .failure(error)),
-//            .completed(400)
-//        ])
-//    }
-//
-//    func testRemoveBookmarks() {
-//        // Arrange
-//        let data = createBookmarksMock([FormattedWord("test2"), FormattedWord("test3")])
-//        let viewModel = BookmarksViewModel(bookmarks: data,
-//                                           player: PlayerManagerStub(),
-//                                           spotlight: createSpotlightMock())
-//        let output = viewModel.transform(from: BookmarksViewModel.Input(search: Driver.never(),
-//                                                                        playUrl: Driver.never(),
-//                                                                        removeBookmark: inputBookmarks,
-//                                                                        restore: Driver.never()))
-//        output.bookmarks.drive(bookmarks)
-//            .disposed(by: disposeBag)
-//
-//        // Act
-//        scheduler.start()
-//
-//        // Assert
-//        XCTAssertEqual(bookmarks.events, [
-//            .next(150, .success([FormattedWord("test3")])),
-//            .next(200, .success([]))
-//        ])
-//    }
-//
-//    func testErrorNilViewModel() {
-//        // Arrange
-//        let data = createBookmarksMock([FormattedWord("test2"), FormattedWord("test3")])
-//        var viewModel: BookmarksViewModel? = BookmarksViewModel(bookmarks: data,
-//                                           player: MockFactory.createMockPlayerManager(),
-//                                           spotlight: createSpotlightMock())
-//        let output = viewModel!.transform(from: BookmarksViewModel.Input(search: inputWords,
-//                                                            playUrl: inputUrls,
-//                                                            removeBookmark: inputBookmarks,
-//                                                            restore: inputWords))
-//        disposeBag.insert(
-//            output.bookmarks.drive(bookmarks),
-//            output.played.drive(played)
-//        )
-//
-//        // Act
-//        viewModel = nil
-//        scheduler.start()
-//
-//        // Assert
-//        XCTAssertEqual(bookmarks.events, [
-//            .next(150, .success([])),
-//            .next(200, .success([])),
-//            .next(200, .success([])),
-//            .next(250, .success([])),
-//            .next(250, .success([])),
-//            .next(300, .success([])),
-//            .next(300, .success([]))
-//        ])
-//        XCTAssertEqual(played.events, [
-//            .next(150, .success(false)),
-//            .next(200, .success(false)),
-//            .next(300, .success(false)),
-//            .completed(400)
-//        ])
-//    }
-//
-//    private func createBookmarksMock(_ data: [FormattedWord]) -> AnyStorageRepository<FormattedWord> {
-//        return AnyStorageRepository<FormattedWord>(wrapped: MockFactory.createMockStorageRepository(data: data));
-//    }
-//
-//    private func createSpotlightMock() -> ExternalCacheService {
-//        let mock = MockExternalCacheService()
-//        stub(mock) { stub in
-//            when(stub.getTitle(from: anyString())).then { $0 }
-//            when(stub.index(data: any())).thenReturn(Observable.just(.success(())))
-//        }
-//        return mock
-//    }
-//}
+
+@testable import YASD
+
+import XCTest
+import RxSwift
+import RxCocoa
+import RxTest
+import Cuckoo
+
+class BookmarksViewModelTests: XCTestCase {
+    enum TestError: Error {
+        case someError
+    }
+
+    let disposeBag = DisposeBag()
+    let scheduler = TestScheduler(initialClock: 0)
+
+    func testSearch() {
+        // Arrange
+        let testString = "test"
+        let mockSearchBookmark = createMockSearchBookmark()
+        let mockChangedBookmark = createMockChangedBookmarkNever()
+        let viewModel = BookmarksViewModel(searchBookmark: mockSearchBookmark,
+                                           restoreBookmark: createUseCaseStub(),
+                                           removeBookmark: createUseCaseStub(),
+                                           changedBookmark: mockChangedBookmark,
+                                           playSound: createUseCaseStub())
+        let inputWords = scheduler.createHotObservable([
+            .next(200, testString),
+            .completed(400)
+        ]).asDriver(onErrorJustReturn: "")
+        let outputBookmarks = scheduler.createObserver(FormattedWordResult.self)
+        let output = viewModel.transform(from: BookmarksViewModel
+                                            .Input(search: inputWords,
+                                                   playUrl: Driver.never(),
+                                                   removeBookmark: Driver.never(),
+                                                   restore: Driver.never()))
+        output.bookmarks.drive(outputBookmarks)
+            .disposed(by: disposeBag)
+
+        // Act
+        scheduler.start()
+
+        // Assert
+        XCTAssertEqual(outputBookmarks.events, [
+            .next(200, .success([FormattedWord(testString)]))
+        ])
+    }
+
+    func testRestore() {
+        // Arrange
+        let testString = "test"
+        let mockSearchBookmark = createMockSearchBookmark()
+        let mockChangedBookmark = createMockChangedBookmarkNever()
+        let mockRestoreBookmark = createMockRestoreBookmark()
+        let inputRestore = scheduler.createHotObservable([
+            .next(200, testString),
+            .completed(400)
+        ]).asDriver(onErrorJustReturn: "")
+        let outputRestore = scheduler.createObserver(String.self)
+        let outputBookmarks = scheduler.createObserver(Bookmarks.self)
+        let viewModel = BookmarksViewModel(searchBookmark: mockSearchBookmark,
+                                           restoreBookmark: mockRestoreBookmark,
+                                           removeBookmark: createUseCaseStub(),
+                                           changedBookmark: mockChangedBookmark,
+                                           playSound: createUseCaseStub())
+        let output = viewModel.transform(from: BookmarksViewModel
+                                            .Input(search: Driver.never(),
+                                                   playUrl: Driver.never(),
+                                                   removeBookmark: Driver.never(),
+                                                   restore: inputRestore))
+        disposeBag.insert(
+            output.restored.drive(outputRestore),
+            output.bookmarks.drive(outputBookmarks)
+        )
+
+        // Act
+        scheduler.start()
+
+        // Assert
+        XCTAssertEqual(outputRestore.events, [
+            .next(200, testString),
+            .completed(400)
+        ])
+        XCTAssertEqual(outputBookmarks.events, [
+            .next(200, .success([FormattedWord(testString)]))
+        ])
+    }
+    
+    private func createMockSearchBookmark() -> MockAnyUseCase<String, Bookmarks> {
+        let mockSearchBookmark = MockAnyUseCase(wrapped: MockUseCase<String, Bookmarks>())
+        stub(mockSearchBookmark) { stub in
+            when(stub.execute(with: anyString())).then { data in
+                return Observable.just(.success([FormattedWord(data)]))
+            }
+        }
+        return mockSearchBookmark
+    }
+    
+    private func createMockChangedBookmarkNever() -> MockAnyUseCase<Void, Bookmarks> {
+        let mockChangedBookmark = MockAnyUseCase(wrapped: MockUseCase<Void, Bookmarks>())
+        stub(mockChangedBookmark) { stub in
+            when(stub.execute(with: any())).then { data in
+                return Observable.never()
+            }
+        }
+        return mockChangedBookmark
+    }
+    
+    private func createMockRestoreBookmark() -> MockAnyUseCase<String, String> {
+        let mockRestoreBookmark = MockAnyUseCase(wrapped: MockUseCase<String, String>())
+        stub(mockRestoreBookmark) { stub in
+            when(stub.execute(with: any())).then { data in
+                return Observable.just(data)
+            }
+        }
+        return mockRestoreBookmark
+    }
+    
+    private func createUseCaseStub<Input, Output>() -> AnyUseCaseStub<Input, Output> {
+        return AnyUseCaseStub<Input, Output>(wrapped: UseCaseStub())
+    }
+}
