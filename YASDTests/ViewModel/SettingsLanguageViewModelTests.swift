d95777c791c33f8882014def57deee2640af02f4
Vyacheslav Konopkin <robotal76@gmail.com>
2021-05-14 19:18:05 +0300

    Add SettingsLanguageViewModelTests

--- YASDTests/ViewModel/SettingsLanguageViewModelTests.swift
+++ YASDTests/ViewModel/SettingsLanguageViewModelTests.swift
@@ -1,83 +1,76 @@
-////
-////  SettingsLanguageViewModelTests.swift
-////  YASDTests
-////
-////  Created by Vyacheslav Konopkin on 05/08/2019.
-////  Copyright © 2019 yac. All rights reserved.
-////
 //
-//@testable import YASD
+//  SettingsLanguageViewModelTests.swift
+//  YASDTests
 //
-//import XCTest
-//import RxSwift
-//import RxCocoa
-//import RxTest
-//import Cuckoo
+//  Created by Vyacheslav Konopkin on 05/08/2019.
+//  Copyright © 2019 yac. All rights reserved.
 //
-//class SettingsLanguageViewModelTests: XCTestCase {
-//    let disposeBag = DisposeBag()
-//    
-//    func testSettingsLanguageSelect() {
-//        // Arrange
-//        let testLanguage = Language(name: "test", code: "test")
-//        let scheduler = TestScheduler(initialClock: 0)
-//        let inputLanguages = scheduler.createHotObservable([
-//            .next(150, testLanguage.name)
-//        ]).asDriver(onErrorJustReturn: "")
-//        let res = scheduler.createObserver([SettingsLanguageItem].self)
-//        let viewModel = SettingsLanguageViewModel(settings: createSettingsLanguageService(testLanguage))
-//        let output = viewModel.transform(from: SettingsLanguageViewModel.Input(search: Driver.just(""),
-//                                                                               select: inputLanguages))
-//        output.languages.drive(res)
-//            .disposed(by: disposeBag)
-//
-//        // Act
-//        scheduler.start()
-//
-//        // Assert
-//        XCTAssertEqual(res.events, [
-//            .next(0, [ SettingsLanguageItem(selected: false, language: testLanguage) ]),
-//            .next(150, [ SettingsLanguageItem(selected: false, language: testLanguage) ])
-//        ])
-//    }
-//    
-//    func testSettingsLanguageSearch() {
-//        // Arrange
-//        let testLanguage = Language(name: "test", code: "test")
-//        let scheduler = TestScheduler(initialClock: 0)
-//        let inputLanguages = scheduler.createHotObservable([
-//            .next(150, testLanguage.name)
-//        ]).asDriver(onErrorJustReturn: "")
-//        let res = scheduler.createObserver([SettingsLanguageItem].self)
-//        let viewModel = SettingsLanguageViewModel(settings: createSettingsLanguageService(testLanguage))
-//        let output = viewModel.transform(from: SettingsLanguageViewModel.Input(search: inputLanguages,
-//                                                                               select: Driver.never()))
-//        output.languages.drive(res).disposed(by: disposeBag)
-//        
-//        // Act
-//        scheduler.start()
-//        
-//        // Assert
-//        XCTAssertEqual(res.events, [
-//            .next(150, [ SettingsLanguageItem(selected: false, language: testLanguage) ])
-//        ])
-//    }
-//
-//    
-//    private func createParametersStorageStub(_ language: Language) -> ParametersStorageStub {
-//        DefaultValueRegistry.register(value: language, forType: Language.self)
-//        let stub = ParametersStorageStub(storage: StorageStub(), language: language)
-//        return stub
-//    }
-//    
-//    private func createSettingsLanguageService(_ language: Language) -> MockSettingsLanguageService {
-//        let mock = MockSettingsLanguageService(parameters: createParametersStorageStub(Language(name: "test", code: "test")))
-//        stub(mock) { stub in
-//            when(stub.update(with: any())).thenReturn(Observable.just(.success(true)))
-//            when(stub.get(with: any())).thenReturn(Observable.just(
-//                .success([ SettingsLanguageItem(selected: false, language: language) ]
-//                )))
-//        }
-//        return mock
-//    }
-//}
+
+@testable import YASD
+
+import XCTest
+import RxSwift
+import RxCocoa
+import RxTest
+import Cuckoo
+
+class SettingsLanguageViewModelTests: XCTestCase {
+    enum TestError: Error {
+        case someError
+    }
+    
+    let disposeBag = DisposeBag()
+    let scheduler = TestScheduler(initialClock: 0)
+
+    func testGetLanguage() {
+        // Arrange
+        let testLanguage = "test"
+        let outputLanguage = createGetLanguageObserver(testLanguage)
+        
+        // Act
+        scheduler.start()
+        
+        // Assert
+        XCTAssertEqual(outputLanguage.events, [
+            .next(0, testLanguage),
+            .completed(0)
+        ])
+    }
+    
+    func testGetLanguageError() {
+        // Arrange
+        let outputLanguage = createGetLanguageObserver("error")
+        
+        // Act
+        scheduler.start()
+        
+        // Assert
+        XCTAssertEqual(outputLanguage.events, [
+            .next(0, Language.defaultLanguage.name),
+            .completed(0)
+        ])
+    }
+    
+    private func createGetLanguageObserver(_ testLanguage: String) -> TestableObserver<String> {
+        let viewModel = SettingsViewModel(getLanguage: createMockGetLanguage(testLanguage))
+        let outputLanguage = scheduler.createObserver(String.self)
+        let output = viewModel.transform(from: SettingsViewModel.Input())
+        disposeBag.insert(
+            output.selectedLanguage.drive(outputLanguage)
+        )
+        return outputLanguage
+    }
+    
+    private func createMockGetLanguage(_ language: String) -> MockAnyUseCase<Void, String> {
+        let mockGetLanguage = MockAnyUseCase(wrapped: MockUseCase<Void, String>())
+        stub(mockGetLanguage) { stub in
+            when(stub.execute(with: any())).then { _ in
+                if language == "error" {
+                    return Observable.error(TestError.someError)
+                }
+                return Observable.just(language)
+            }
+        }
+        return mockGetLanguage
+    }
+}
