ef8e64aacddff9c1eb21bdde5ddafbd9e2644c9c
Vyacheslav Konopkin <robotal76@gmail.com>
2021-06-17 17:38:28 +0300

    Add tests
    - Regroup tests
    - Add GetTextAboutUseCaseTests
    - Add PlaySoundUseCaseTests

---
+++ YASDTests/Domain/UseCase/About/GetTextAboutUseCaseTests.swift
@@ -0,0 +1,52 @@
+//
+//  GetTextAboutUseCaseTests.swift
+//  YASDTests
+//
+//  Created by Vyacheslav Konopkin on 17.06.2021.
+//  Copyright © 2021 yac. All rights reserved.
+//
+@testable import YASD
+
+import XCTest
+import RxSwift
+import RxCocoa
+import RxTest
+import Cuckoo
+
+class GetTextAboutUseCaseTests: XCTestCase {
+    enum TestError: Error {
+        case someError
+    }
+    
+    let disposeBag = DisposeBag()
+    let scheduler = TestScheduler(initialClock: 0)
+    
+    func testExecute() {
+        // Arrange
+        let testValue = "test"
+        let outputText = scheduler.createObserver(NSAttributedString.self)
+        let useCase = GetTextAboutUseCase(repository: createMockAboutTextRepository(testValue))
+        let res = useCase.execute(with: ())
+        res.bind(to: outputText)
+            .disposed(by: disposeBag)
+        
+        // Act
+        scheduler.start()
+        
+        // Assert
+        XCTAssertEqual(outputText.events, [
+            .next(0, NSAttributedString(string: testValue)),
+            .completed(0)
+        ])
+    }
+    
+    private func createMockAboutTextRepository(_ value: String) -> MockAboutTextRepository {
+        let mock = MockAboutTextRepository()
+        stub(mock) { stub in
+            when(stub.getText()).then { _ in
+                return .just(NSAttributedString(string: value))
+            }
+        }
+        return mock
+    }
+}
---
+++ YASDTests/Domain/UseCase/PlaySound/PlaySoundUseCaseTests.swift
@@ -0,0 +1,51 @@
+//
+//  PlaySoundUseCaseTests.swift
+//  YASDTests
+//
+//  Created by Vyacheslav Konopkin on 17.06.2021.
+//  Copyright © 2021 yac. All rights reserved.
+//
+@testable import YASD
+
+import XCTest
+import RxSwift
+import RxCocoa
+import RxTest
+import Cuckoo
+
+class PlaySoundUseCaseTests: XCTestCase {
+    enum TestError: Error {
+        case someError
+    }
+    
+    let disposeBag = DisposeBag()
+    let scheduler = TestScheduler(initialClock: 0)
+    
+    func testExecute() {
+        // Arrange
+        let outputPlayer = scheduler.createObserver(PlayerManagerResult.self)
+        let useCase = PlaySoundUseCase(player: createMockPlayManager())
+        let res = useCase.execute(with: "test")
+        res.bind(to: outputPlayer)
+            .disposed(by: disposeBag)
+
+        // Act
+        scheduler.start()
+
+        // Assert
+        XCTAssertEqual(outputPlayer.events, [
+            .next(0, .success(true)),
+            .completed(0)
+        ])
+    }
+    
+    private func createMockPlayManager() -> MockPlayerManager {
+        let mock = MockPlayerManager()
+        stub(mock) { stub in
+            when(stub.playSound(with: any())).then { _ in
+                return .just(.success(true))
+            }
+        }
+        return mock
+    }
+}
