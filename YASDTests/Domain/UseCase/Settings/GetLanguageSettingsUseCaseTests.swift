c51ae233c9e3f3f66a0e6421e1da84c2b3a4bd8e
Vyacheslav Konopkin <robotal76@gmail.com>
2021-06-17 18:06:32 +0300

    Add GetLanguageSettingsUseCaseTests

---
+++ YASDTests/Domain/UseCase/Settings/GetLanguageSettingsUseCaseTests.swift
@@ -0,0 +1,54 @@
+//
+//  GetLanguageSettingsUseCaseTests.swift
+//  YASDTests
+//
+//  Created by Vyacheslav Konopkin on 17.06.2021.
+//  Copyright © 2021 yac. All rights reserved.
+//
+@testable import YASD
+
+import XCTest
+import RxSwift
+import RxCocoa
+import RxTest
+import Cuckoo
+
+class GetLanguageSettingsUseCaseTests: XCTestCase {
+    enum TestError: Error {
+        case someError
+    }
+    
+    let disposeBag = DisposeBag()
+    let scheduler = TestScheduler(initialClock: 0)
+    
+    func testExecute() {
+        // Arrange
+        let testValue = "test"
+        let outputLanguage = scheduler.createObserver(String.self)
+        let useCase = GetLanguageSettingsUseCase(repository: createMockSettingsRepository(testValue))
+        let res = useCase.execute(with: ())
+        res.bind(to: outputLanguage)
+            .disposed(by: disposeBag)
+
+        // Act
+        scheduler.start()
+
+        // Assert
+        XCTAssertEqual(outputLanguage.events, [
+            .next(0, testValue)
+        ])
+    }
+    
+    private func createMockSettingsRepository(_ testValue: String) -> MockSettingsRepository {
+        let mock = MockSettingsRepository()
+        stub(mock) { stub in
+            when(stub.getLanguage()).then { _ in
+                return .just(Language(name: testValue, code: testValue))
+            }
+            when(stub.getLanguageBehaviour()).then { _ -> PublishSubject<Language> in
+                return PublishSubject<Language>()
+            }
+        }
+        return mock
+    }
+}
