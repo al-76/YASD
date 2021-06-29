3ee46ddfe8c30cf1d1501c19e6f50fbf1b9e29b5
Vyacheslav Konopkin <robotal76@gmail.com>
2021-06-29 14:29:27 +0300

    Add AddBookmarkUseCaseTests

---
+++ YASDTests/Domain/UseCase/Bookmarks/AddBookmarkUseCaseTests.swift
@@ -0,0 +1,41 @@
+//
+//  AddBookmarkUseCaseTests.swift
+//  YASDTests
+//
+//  Created by Vyacheslav Konopkin on 29.06.2021.
+//  Copyright © 2021 yac. All rights reserved.
+//
+
+@testable import YASD
+
+import XCTest
+import RxSwift
+import RxCocoa
+import RxTest
+import Cuckoo
+
+class AddBookmarkUseCaseTests: XCTestCase {
+    let disposeBag = DisposeBag()
+    let scheduler = TestScheduler(initialClock: 0)
+    
+    func testExecute() {
+        // Arrange
+        let outputItems = scheduler.createObserver(StorageServiceResult.self)
+        let useCase = AddBookmarkUseCase(bookmarks: MockFactory.createFormattedWordStorageRepository("test", PublishSubject<Bool>()))
+        let res = useCase.execute(with: FormattedWord("test"))
+        disposeBag.insert(
+            res.bind(to: outputItems)
+        )
+        
+        // Act
+        scheduler.start()
+
+        // Assert
+        XCTAssertEqual(outputItems.events, [
+            .next(0, .success(true)),
+            .completed(0)
+        ])
+    }
+}
+    
+    
