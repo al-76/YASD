8b3572ddf2d5bd72af1b8f3312f7c04a12e157f5
Vyacheslav Konopkin <robotal76@gmail.com>
2021-06-29 12:53:42 +0300

    Add RemoveBookmarkByIndexUseCaseTests

--- YASDTests/Domain/UseCase/Bookmarks/RemoveBookmarkByIndexUseCaseTests.swift
+++ YASDTests/Domain/UseCase/Bookmarks/RemoveBookmarkByIndexUseCaseTests.swift
@@ -1,8 +1,8 @@
 //
-//  SearchBookmarksUseCaseTests.swift
+//  RemoveBookmarkByIndexUseCaseTests.swift
 //  YASDTests
 //
-//  Created by Vyacheslav Konopkin on 25.06.2021.
+//  Created by Vyacheslav Konopkin on 29.06.2021.
 //  Copyright © 2021 yac. All rights reserved.
 //
 
@@ -14,26 +14,25 @@ import RxCocoa
 import RxTest
 import Cuckoo
 
-class SearchBookmarksUseCaseTests: XCTestCase {
+class RemoveBookmarkByIndexUseCaseTests: XCTestCase {
     let disposeBag = DisposeBag()
     let scheduler = TestScheduler(initialClock: 0)
-    
+
     func testExecute() {
         // Arrange
-        let testValue = "test"
-        let outputItems = scheduler.createObserver(Bookmarks.self)
-        let useCase = SearchBookmarkUseCase(bookmarks: MockFactory.createFormattedWordStorageRepository(testValue))
-        let res = useCase.execute(with: "")
+        let outputItems = scheduler.createObserver(StorageServiceResult.self)
+        let useCase = RemoveBookmarkByIndexUseCase(bookmarks: MockFactory.createFormattedWordStorageRepository("test"))
+        let res = useCase.execute(with: 0)
         disposeBag.insert(
             res.bind(to: outputItems)
         )
         
         // Act
         scheduler.start()
-
+        
         // Assert
         XCTAssertEqual(outputItems.events, [
-            .next(0, .success([FormattedWord(testValue)])),
+            .next(0, .success(true)),
             .completed(0)
         ])
     }
