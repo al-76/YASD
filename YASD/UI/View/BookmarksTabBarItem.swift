c22c71125fd4701aabd84e18e422594787e5e4c9
Vyacheslav Konopkin <robot.al76@gmail.com>
2021-07-07 17:29:02 +0300

    Fix icons for iOS < 13

---
+++ YASD/UI/View/BookmarksTabBarItem.swift
@@ -0,0 +1,22 @@
+//
+//  BookmarksTabBarItem.swift
+//  YASD
+//
+//  Created by Jobbare on 07.07.2021.
+//  Copyright © 2021 yac. All rights reserved.
+//
+
+import UIKit
+
+class BookmarksTabBarItem: UITabBarItem {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        if #available(iOS 13, *) {
+            // nothing
+        } else {
+            // XXX: there's weird bug with assets - bookmark loads as bookmark.fill
+            image = UIImage(named: "fill_bookmark")
+        }
+    }
+}
