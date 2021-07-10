bd551d339553e10eeda771bedd2abc61dd795095
Vyacheslav Konopkin <robot.al76@gmail.com>
2021-07-10 14:13:25 +0300

    Add getting the version from 'CFBundleShortVersionString' to the about text

--- YASD/Data/DefaultAboutTextRepository.swift
+++ YASD/Data/DefaultAboutTextRepository.swift
@@ -24,7 +24,8 @@ class DefaultAboutTextRepository: AboutTextRepository {
         return Observable.create { [weak self] observer in
             if let self = self {
                 let text = self.markdown
-                    .parse(data: NSLocalizedString("textAbout", comment: ""))
+                    .parse(data: NSLocalizedString("textAbout", comment: "")
+                            .replacingOccurrences(of: "%s", with: self.getAppVersion()))
                 observer.onNext(.success(text))
             } else {
                 observer.onNext(.failure(DefaultAboutTextRepositoryError.noContext))
@@ -32,5 +33,10 @@ class DefaultAboutTextRepository: AboutTextRepository {
             observer.onCompleted()
             return Disposables.create {}
         }
+    }
+
+    private func getAppVersion() -> String {
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString")
+        return (version as? String) ?? "1.0.0"
     }
 }
